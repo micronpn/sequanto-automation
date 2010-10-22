57c1d08587ce091fb8d3bc3202ac7f399fa1058e
rasmus.toftdahl.olesen <rasmus.toftdahl.olesen@76fca13c-22ec-11df-bcc3-9d5e0fc24faa>
2010-10-22 10:54:52 +0000

    Add xml output to unit test

--- trunk/server/c/tests/main.c
+++ trunk/server/c/tests/main.c
@@ -5,6 +5,7 @@ int main ( int _argc, char * argv[] )
     int number_failed;
     SRunner *sr = srunner_create ( (Suite*) value_suite() );
     srunner_add_suite ( sr, (Suite*) parser_suite() );
+    srunner_set_xml ( sr, "output.xml" );
     srunner_run_all (sr, CK_NORMAL);
     number_failed = srunner_ntests_failed (sr);
     srunner_free (sr);
--- trunk/server/c/tests/test_value.c
+++ trunk/server/c/tests/test_value.c
@@ -32,7 +32,56 @@ START_TEST(test_value_integer)
 }
 END_TEST
 
+START_TEST(test_value_boolean)
+{
+    SQValue value;
+    sq_value_init ( &value );
+    
+    sq_value_boolean ( &value, SQ_TRUE );
+    
+    fail_unless ( value.m_type == VALUE_TYPE_BOOLEAN );
+    fail_unless ( value.Value.m_booleanValue == SQ_TRUE );
+}
+END_TEST
+
+START_TEST(test_value_float)
+{
+    SQValue value;
+    sq_value_init ( &value );
+    
+    sq_value_float ( &value, -3.14 );
+    
+    fail_unless ( value.m_type == VALUE_TYPE_FLOAT );
+    fail_unless ( value.Value.m_floatValue == -3.14f );
+}
+END_TEST
+
+START_TEST(test_value_string)
+{
+    SQValue value;
+    sq_value_init ( &value );
+    
+    char name[] = "Rasmus Toftdahl Olesen";
+    
+    sq_value_const_string ( &value, name );
+    
+    fail_unless ( value.m_type == VALUE_TYPE_CONST_STRING );
+    ck_assert_str_eq ( value.Value.m_stringValue, name );
+    //    fail_unless ( ((void*) value.Value.m_stringValue) == ((void*) &name) );
+    
+    sq_value_free ( &value );
+    
+    fail_unless ( value.m_type == VALUE_TYPE_NO_VALUE );
+    fail_unless ( value.Value.m_stringValue == NULL );
+    
+    ck_assert_str_eq ( "Rasmus Toftdahl Olesen2", name );
+}
+END_TEST
+
 SQ_TEST_SUITE(value_suite, "SQValue");
 SQ_TEST_CASE(test_value_init);
 SQ_TEST_CASE(test_value_integer);
+SQ_TEST_CASE(test_value_boolean);
+SQ_TEST_CASE(test_value_float);
+SQ_TEST_CASE(test_value_string);
 SQ_END_SUITE();

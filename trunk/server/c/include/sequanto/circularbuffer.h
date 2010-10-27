7024f39e2da049b7cc50d26ecf01c6ada2ae3b1e
rasmus.toftdahl.olesen <rasmus.toftdahl.olesen@76fca13c-22ec-11df-bcc3-9d5e0fc24faa>
2010-10-27 09:02:10 +0000

    Make position counters in circularbuffer volatile, usable on embedded platforms.

--- trunk/server/c/include/sequanto/circularbuffer.h
+++ trunk/server/c/include/sequanto/circularbuffer.h
@@ -25,12 +25,14 @@ extern "C"
 #endif
 
     int sq_circular_buffer_available ( int _size, int _readPosition, int _writePosition );
-    void sq_circular_buffer_push ( int _size, int * _readPosition, int * _writePosition, SQByte * _data, SQByte _value );
-    SQByte sq_circular_buffer_pop ( int _size, int * _readPosition, int * _writePosition, SQByte * _data );
+    SQBool sq_circular_buffer_full ( int _readPosition );
+    void sq_circular_buffer_push ( int _size, volatile int * _readPosition, volatile int * _writePosition, SQByte * _data, SQByte _value );
+    SQByte sq_circular_buffer_pop ( int _size, volatile int * _readPosition, volatile int * _writePosition, SQByte * _data );
 
-#define SQ_CIRCULAR_BUFFER_DEFINE(parm_name, parm_size) struct { int m_size; int m_readPosition; int m_writePosition; SQByte m_data[parm_size]; } parm_name;
+#define SQ_CIRCULAR_BUFFER_DEFINE(parm_name, parm_size) struct { int m_size; volatile int m_readPosition; volatile int m_writePosition; SQByte m_data[parm_size]; } parm_name;
 #define SQ_CIRCULAR_BUFFER_INIT(parm_name, parm_size) parm_name.m_size = parm_size; parm_name.m_readPosition = 0; parm_name.m_writePosition = 0;
 #define SQ_CIRCULAR_BUFFER_AVAILABLE(name) sq_circular_buffer_available ( name.m_size, name.m_readPosition, name.m_writePosition )
+#define SQ_CIRCULAR_BUFFER_FULL(name) (name.m_readPosition == -1 ? SQ_TRUE : SQ_FALSE)
 #define SQ_CIRCULAR_BUFFER_PUSH(name, value) sq_circular_buffer_push ( name.m_size, &name.m_readPosition, &name.m_writePosition, name.m_data, value )
 #define SQ_CIRCULAR_BUFFER_POP(name) sq_circular_buffer_pop ( name.m_size, &name.m_readPosition, &name.m_writePosition, name.m_data )
 

a941085e1df09d0c0f0efb2e84ca038a504daa66
rasmus.toftdahl.olesen <rasmus.toftdahl.olesen@76fca13c-22ec-11df-bcc3-9d5e0fc24faa>
2011-01-13 20:22:29 +0000

    Finally figured out how to make de-construction work.

--- server/qt/src/QtWidgetNode.cxx
+++ server/qt/src/QtWidgetNode.cxx
@@ -1,5 +1,6 @@
 #include <sequanto/QtWidgetNode.h>
 #include <sequanto/QtWrapper.h>
+#include <cassert>
 
 using namespace sequanto::automation;
 
@@ -13,12 +14,23 @@ QtWidgetNode::QtWidgetNode( QWidget * _widget )
 
 void QtWidgetNode::WidgetDestroyed()
 {
-   dynamic_cast<ListNode*>(this->GetParent())->RemoveChild ( GetName() );
+   // If m_widget is NULL we are in the process of being destroyed
+   if ( m_widget != NULL )
+   {
+      m_widget = NULL;
+      dynamic_cast<ListNode*>(this->GetParent())->RemoveChild ( GetName() );
+   }
 }
 
 QtWidgetNode::~QtWidgetNode()
 {
-   m_widget->removeEventFilter ( m_eventFilter );
-   //delete m_eventFilter;
-   m_widget = NULL;
+   // If m_widget is NULL our widget has already been destroyed and we do not need to do anything to un-register ourselves from the widget.
+   if ( m_widget != NULL )
+   {
+      QWidget * widget = m_widget;
+      m_widget = NULL;
+
+      widget->removeEventFilter ( m_eventFilter );
+      delete m_eventFilter;
+   }
 }

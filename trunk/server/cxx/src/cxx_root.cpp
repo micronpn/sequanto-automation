e11f6341d43299cf42b91bbe561b761f6e77c2b2
rasmus.toftdahl.olesen <rasmus.toftdahl.olesen@76fca13c-22ec-11df-bcc3-9d5e0fc24faa>
2011-04-30 08:18:24 +0000

    Return error in cxx_root if the object was not found (message was not handled).

--- trunk/server/cxx/src/cxx_root.cpp
+++ trunk/server/cxx/src/cxx_root.cpp
@@ -1,4 +1,5 @@
 #include <sequanto/automation.h>
+#include <sequanto/protocol.h>
 #include <sequanto/listnode.h>
 #include <sequanto/tree.h>
 
@@ -17,54 +18,77 @@ sequanto::automation::ListNode * sequanto::automation::sq_get_cxx_root ()
    return s_root;
 }
 
+static const char COULD_NOT_FIND_GIVEN_OBJECT[] SQ_CONST_VARIABLE = "Could not find given object.";
+
 extern "C"
 {
    void sq_parser_info ( SQParser * _parser, SQStream * _stream, const char * const _objectPath )
    {
       SQ_UNUSED_PARAMETER(_parser);
       
-      s_tree.HandleInfo ( _stream, _objectPath );
+      if ( s_tree.HandleInfo ( _stream, _objectPath ) == SQ_FALSE )
+      {
+         sq_protocol_write_failure_with_text_message ( _stream, COULD_NOT_FIND_GIVEN_OBJECT );
+      }
    }
 
    void sq_parser_list ( SQParser * _parser, SQStream * _stream, const char * const _objectPath )
    {
       SQ_UNUSED_PARAMETER(_parser);
       
-      s_tree.HandleList ( _stream, _objectPath );
+      if ( s_tree.HandleList ( _stream, _objectPath ) == SQ_FALSE )
+      {
+         sq_protocol_write_failure_with_text_message ( _stream, COULD_NOT_FIND_GIVEN_OBJECT );
+      }
    }
 
    void sq_parser_call ( SQParser * _parser, SQStream * _stream, const char * const _objectPath, const SQValue * const _inputValues, int _numberOfValues )
    {
       SQ_UNUSED_PARAMETER(_parser);
       
-      s_tree.HandleCall ( _stream, _objectPath, _inputValues, _numberOfValues );
+      if ( s_tree.HandleCall ( _stream, _objectPath, _inputValues, _numberOfValues ) == SQ_FALSE )
+      {
+         sq_protocol_write_failure_with_text_message ( _stream, COULD_NOT_FIND_GIVEN_OBJECT );
+      }
    }
 
    void sq_parser_property_get ( SQParser * _parser, SQStream * _stream, const char * const _objectPath )
    {
       SQ_UNUSED_PARAMETER(_parser);
       
-      s_tree.HandleGet ( _stream, _objectPath );
+      if ( s_tree.HandleGet ( _stream, _objectPath ) == SQ_FALSE )
+      {
+         sq_protocol_write_failure_with_text_message ( _stream, COULD_NOT_FIND_GIVEN_OBJECT );
+      }
    }
    
    void sq_parser_property_set ( SQParser * _parser, SQStream * _stream, const char * const _objectPath, const SQValue * _value )
    {
       SQ_UNUSED_PARAMETER(_parser);
       
-      s_tree.HandleSet ( _stream, _objectPath, _value );
+      if ( s_tree.HandleSet ( _stream, _objectPath, _value ) == SQ_FALSE )
+      {
+         sq_protocol_write_failure_with_text_message ( _stream, COULD_NOT_FIND_GIVEN_OBJECT );
+      }
    }
 
    void sq_parser_disable ( SQParser * _parser, SQStream * _stream, const char * const _objectPath )
    {
       SQ_UNUSED_PARAMETER(_parser);
       
-      s_tree.HandleDisable ( _stream, _objectPath );
+      if ( s_tree.HandleDisable ( _stream, _objectPath ) == SQ_FALSE )
+      {
+         sq_protocol_write_failure_with_text_message ( _stream, COULD_NOT_FIND_GIVEN_OBJECT );
+      }
    }
 
    void sq_parser_enable ( SQParser * _parser, SQStream * _stream, const char * const _objectPath )
    {
       SQ_UNUSED_PARAMETER(_parser);
       
-      s_tree.HandleEnable ( _stream, _objectPath );
+      if (s_tree.HandleEnable ( _stream, _objectPath ) == SQ_FALSE )
+      {
+         sq_protocol_write_failure_with_text_message ( _stream, COULD_NOT_FIND_GIVEN_OBJECT );
+      }
    }
 }

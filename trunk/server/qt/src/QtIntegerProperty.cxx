7080b06062bbc2aa512952f660f04e1f08e4e578
rasmus.toftdahl.olesen <rasmus.toftdahl.olesen@76fca13c-22ec-11df-bcc3-9d5e0fc24faa>
2011-06-06 08:07:27 +0000

    Add QtIntegerPropertyWithAlternateName property, mimicking the code in QtStringPropertyWithAlternateName.

--- trunk/server/qt/src/QtIntegerProperty.cxx
+++ trunk/server/qt/src/QtIntegerProperty.cxx
@@ -9,8 +9,14 @@ QtIntegerProperty::QtIntegerProperty ( const std::string & _name, QObject * _obj
      m_object ( _object ),
      m_notifyAdapter ( NULL )
 {
-   m_notifyAdapter = QtPropertyChangedNotificationAdapter::ConnectIfPossible ( _object, _name, this );
+   m_notifyAdapter = QtPropertyChangedNotificationAdapter::ConnectIfPossible ( _object, QtPropertyName(), this );
 }
+
+const std::string & QtIntegerProperty::QtPropertyName () const
+{
+   return GetName();
+}
+
 
 void QtIntegerProperty::PropertyChanged ()
 {
@@ -21,7 +27,7 @@ int QtIntegerProperty::GetValue ()
 {
    assert ( m_object != NULL );
      
-   QVariant value = QtWrapper::GetPropertyValue ( m_object, GetName() );
+   QVariant value = QtWrapper::GetPropertyValue ( m_object, QtPropertyName() );
    if ( value.isValid() )
    {
       return value.toInt();
@@ -29,7 +35,7 @@ int QtIntegerProperty::GetValue ()
    else
    {
       qDebug() << "    value is NOT valid.";
-       
+      
       return -1;
    }
 }
@@ -37,7 +43,7 @@ int QtIntegerProperty::GetValue ()
 void QtIntegerProperty::SetValue ( int _newValue )
 {
    QVariant variantValue ( _newValue );
-   m_object->setProperty(GetName().c_str(), variantValue );
+   m_object->setProperty(QtPropertyName().c_str(), variantValue );
 }
 
 QtIntegerProperty::~QtIntegerProperty()
@@ -47,4 +53,38 @@ QtIntegerProperty::~QtIntegerProperty()
       delete m_notifyAdapter;
       m_notifyAdapter = NULL;
    }
+}
+
+QtIntegerPropertyWithAlternateName::QtIntegerPropertyWithAlternateName ( const std::string & _propertyName, QObject * _object, const std::string & _qtPropertyName )
+   : QtIntegerProperty(_propertyName, _object)
+{
+   init ( _propertyName, _object, _qtPropertyName, _qtPropertyName );
+}
+
+QtIntegerPropertyWithAlternateName::QtIntegerPropertyWithAlternateName ( const std::string & _propertyName, QObject * _object, const std::string & _qtPropertyName, const std::string & _qtPropertyToUseForNotification  )
+   : QtIntegerProperty(_propertyName, _object)
+{
+   init ( _propertyName, _object, _qtPropertyName, _qtPropertyToUseForNotification );
+}
+
+const std::string & QtIntegerPropertyWithAlternateName::QtPropertyName() const
+{
+   return m_qtPropertyName;
+}
+
+void QtIntegerPropertyWithAlternateName::init ( const std::string & _propertyName, QObject * _object, const std::string & _qtPropertyName, const std::string & _qtPropertyToUseForNotification )
+{
+   SQ_UNUSED_PARAMETER(_propertyName);
+   
+   m_qtPropertyName = _qtPropertyName;
+   // We need to subscribe to the notification again, because the base constructor does not use our QtPropertyName() implementation when connecting.
+   if ( m_notifyAdapter != NULL )
+   {
+      delete m_notifyAdapter;
+   }
+   m_notifyAdapter = QtPropertyChangedNotificationAdapter::ConnectIfPossible ( _object, _qtPropertyToUseForNotification, this );
+}
+
+QtIntegerPropertyWithAlternateName::~QtIntegerPropertyWithAlternateName ()
+{
 }

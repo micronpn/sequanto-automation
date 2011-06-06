5e8a3ba5f806ac222f110e0b5c2a7a1e83cb9f3a
rasmus.toftdahl.olesen <rasmus.toftdahl.olesen@76fca13c-22ec-11df-bcc3-9d5e0fc24faa>
2011-06-06 08:42:39 +0000

    Simplify QtActiveWindowProperty class by basing it on ReadOnlyStringPropertyNode.

--- trunk/server/qt/src/QtActiveWindowProperty.cxx
+++ trunk/server/qt/src/QtActiveWindowProperty.cxx
@@ -6,75 +6,41 @@
 
 using namespace sequanto::automation;
 
+const std::string QtActiveWindowProperty::NO_ACTIVE_WINDOW ( "" );
+
 QtActiveWindowProperty::QtActiveWindowProperty ()
-   : PropertyNode ( SQ_UI_NODE_ACTIVE_WINDOW ),
-     m_previousActiveWindow ( "<NULL>" )
+   : ReadOnlyStringPropertyNode ( SQ_UI_NODE_ACTIVE_WINDOW ),
+     m_currentActiveWindow ( "<NULL>" )
 {
 }
 
-void QtActiveWindowProperty::InternalGet ( SQValue * _outputValue, bool _sendUpdateIfNeeded )
+const std::string & QtActiveWindowProperty::GetValue()
 {
-   static std::string NO_ACTIVE_WINDOW ( "" );
-
-   std::string newActiveWindow;
    QWidget * activeWindow = QApplication::activeWindow();
+   
    if ( activeWindow == NULL )
    {
-      newActiveWindow = NO_ACTIVE_WINDOW;
+      m_currentActiveWindow = NO_ACTIVE_WINDOW;
    }
    else
    {
-      newActiveWindow = QtWrapper::GetObjectName(activeWindow);
+      m_currentActiveWindow = QtWrapper::GetObjectName(activeWindow);
    }
-
-   if ( _outputValue != NULL )
-   {
-      sq_value_string_copy ( _outputValue, newActiveWindow.c_str() );
-   }
-
-   if ( newActiveWindow != m_previousActiveWindow )
-   {
-      m_previousActiveWindow = newActiveWindow;
-
-      if ( _sendUpdateIfNeeded )
-      {
-         // Re-use _outputValue
-         if( _outputValue != NULL )
-         {
-            PropertyNode::SendUpdate ( *_outputValue );
-         }
-         else
-         {
-            SQValue value;
-            sq_value_init ( &value );
-            sq_value_const_string ( &value, m_previousActiveWindow.c_str() );
-            PropertyNode::SendUpdate ( value );
-            sq_value_free ( &value );
-         }
-      }
-   }
-}
-
-const NodeInfo & QtActiveWindowProperty::Info () const
-{
-   return this->GetReadOnlyStringNodeInfo();
-}
-
-void QtActiveWindowProperty::HandleGet ( SQValue & _outputValue )
-{
-   InternalGet ( &_outputValue, false );
-}
-
-void QtActiveWindowProperty::HandleSet ( const SQValue * const _value )
-{
-   SQ_UNUSED_PARAMETER(_value);
    
-   throw std::runtime_error ( "Can not set the active window" );
+   return m_currentActiveWindow;
 }
 
 void QtActiveWindowProperty::TrySendUpdate ()
 {
-   InternalGet ( NULL, true );
+   std::string previousActiveWindow ( m_currentActiveWindow );
+   
+   // Calling GetValue updates m_currentActiveWindow
+   GetValue();
+   
+   if ( m_currentActiveWindow != previousActiveWindow )
+   {
+      PropertyNode::SendUpdate ();
+   }
 }
 
 QtActiveWindowProperty::~QtActiveWindowProperty()

1fd0d05c41ecb334c07df06415eeae22648510ad
rasmus.toftdahl.olesen@gmail.com <rasmus.toftdahl.olesen@gmail.com@76fca13c-22ec-11df-bcc3-9d5e0fc24faa>
2012-10-15 14:29:11 +0000

    Implement the missing new x, y, width, height, role and process_id things in cspi 1.0 version of the automation. Also start to strdup all strings returned by cspi-1.0 - the strings returned cannot be free'ed by a simple call to free - they have to be free'ed using SPI_freeString.

--- trunk/server/c/applications/machine_automation/windows_cspi1.0.c
+++ trunk/server/c/applications/machine_automation/windows_cspi1.0.c
@@ -48,10 +48,108 @@ void windows_unref ( SQByteArray * _pointer )
    Accessible_unref ( accessibleObject );
 }
 
+char * windows_string ( char * _str )
+{
+    char * ret = strdup(_str);
+    SPI_freeString ( _str );
+    return ret;
+}
+
 char * windows_name ( SQByteArray * _pointer )
 {
+   Accessible * accessibleObject;
+   accessibleObject = windows_to_accessible(_pointer);
+   return windows_string ( Accessible_getName ( accessibleObject ) );
+}
+
+int windows_process_id ( SQByteArray * _pointer )
+{
    Accessible * accessibleObject = windows_to_accessible(_pointer);
-   return Accessible_getName ( accessibleObject );
+   AccessibleApplication * application = Accessible_getHostApplication(accessibleObject);
+   long ret = AccessibleApplication_getID ( application );
+   AccessibleApplication_unref ( application );
+   return ret;
+}
+
+int windows_x ( SQByteArray * _pointer )
+{
+   long int x = -1;
+   long int y = -1;
+   Accessible * accessibleObject = windows_to_accessible(_pointer);
+   if ( Accessible_isComponent(accessibleObject) == SPI_TRUE )
+   {
+       AccessibleComponent * component = Accessible_getComponent(accessibleObject);
+       AccessibleComponent_getPosition (component, &x, &y, SPI_COORD_TYPE_SCREEN);
+   }
+   return x;
+}
+
+int windows_y ( SQByteArray * _pointer )
+{
+   long int x = -1;
+   long int y = -1;
+   Accessible * accessibleObject = windows_to_accessible(_pointer);
+   if ( Accessible_isComponent(accessibleObject) == SPI_TRUE )
+   {
+       AccessibleComponent * component = Accessible_getComponent(accessibleObject);
+       AccessibleComponent_getPosition (component, &x, &y, SPI_COORD_TYPE_SCREEN);
+   }
+   return y;
+}
+
+int windows_width ( SQByteArray * _pointer )
+{
+   long int width = 0;
+   long int height = 0;
+   Accessible * accessibleObject = windows_to_accessible(_pointer);
+   if ( Accessible_isComponent(accessibleObject) == SPI_TRUE )
+   {
+       AccessibleComponent * component = Accessible_getComponent(accessibleObject);
+       AccessibleComponent_getSize (component, &width, &height);
+   }
+   return width;
+}
+
+int windows_height ( SQByteArray * _pointer )
+{
+   long int width = 0;
+   long int height = 0;
+   Accessible * accessibleObject = windows_to_accessible(_pointer);
+   if ( Accessible_isComponent(accessibleObject) == SPI_TRUE )
+   {
+       AccessibleComponent * component = Accessible_getComponent(accessibleObject);
+       AccessibleComponent_getSize (component, &width, &height);
+   }
+   return height;
+}
+
+char * windows_role ( SQByteArray * _pointer )
+{  
+   Accessible * accessibleObject = windows_to_accessible(_pointer);
+   return windows_string ( Accessible_getRoleName (accessibleObject) );
+}
+
+char * windows_text ( SQByteArray * _pointer )
+{  
+   char * text = NULL;
+   Accessible * accessibleObject = windows_to_accessible(_pointer);
+   if ( Accessible_isText(accessibleObject) == SPI_TRUE )
+   {
+       AccessibleText * textObj = Accessible_getText(accessibleObject);
+       long count = AccessibleText_getCharacterCount ( textObj );
+       text = AccessibleText_getText ( textObj, 0, count );
+       AccessibleText_unref ( textObj );
+       return windows_string(text);
+   }
+   else if ( Accessible_isValue(accessibleObject) == SPI_TRUE )
+   {
+       AccessibleValue * valueObj = Accessible_getValue(accessibleObject);
+       
+       gdouble current = AccessibleValue_getCurrentValue ( valueObj );
+       
+       return g_strdup_printf("%f", current);
+   }
+   return strdup("");
 }
 
 long windows_children ( SQByteArray * _pointer )

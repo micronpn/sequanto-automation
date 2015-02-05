ec841e5b93ce08dd015f28282e2edcb2c8e28862
rasmus.toftdahl.olesen <rasmus.toftdahl.olesen@76fca13c-22ec-11df-bcc3-9d5e0fc24faa>
2015-02-05 16:00:07 +0000

    Improve hello_world, implement /led and add /iterations and /multiply - remove /hello_world - something bad happens which makes the arduino spew out garbage.

--- arduino/libraries/SequantoAutomation/examples/hello_world/hello_world.ino
+++ arduino/libraries/SequantoAutomation/examples/hello_world/hello_world.ino
@@ -6,37 +6,51 @@
  *
  * BEGIN AUTOMATION
  *
- * function /hello hello_world
+ * function /mult multiply
  * property /led led_get led_set
+ * property /iterations iterations_get
  *
  * END AUTOMATION
  */
 
+const int LED = 13;
+
+int multiply(int a, int b)
+{
+  return a*b;
+}
+
 static SQBool s_led = SQ_FALSE;
 
-SQBool led_get ()
+SQBool led_get ( void )
 {
   return s_led;
 }
 
-void led_get ( SQBool v )
+void led_set ( SQBool v )
 {
   s_led = v;
+  digitalWrite ( LED, s_led == SQ_TRUE ? HIGH : LOW );
 }
 
-char * hello_world ( const char * _name )
+static int s_iterations = 0;
+
+int iterations_get ( void )
 {
-    char * ret = (char*) malloc ( 20 );
-    sprintf ( ret, "Hello %s!", _name );
-    return ret;
+  return s_iterations;
 }
 
 void setup ()
 {
     SequantoAutomation::init();
+    pinMode ( LED, OUTPUT );
+    s_led = SQ_FALSE;
+    digitalWrite ( LED, LOW );
+    s_iterations = 0;
 }
 
 void loop ()
 {
+    s_iterations ++;
     SequantoAutomation::poll();
 }

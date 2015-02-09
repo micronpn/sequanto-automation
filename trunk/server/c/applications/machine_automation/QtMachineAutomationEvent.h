a85a892c99b69ae66662d427c77578ca94acf7c6
rasmus.toftdahl.olesen <rasmus.toftdahl.olesen@76fca13c-22ec-11df-bcc3-9d5e0fc24faa>
2015-02-09 13:03:10 +0000

    Add support /mouse/hold_at and /mouse/release_at to QT and X11.

--- trunk/server/c/applications/machine_automation/QtMachineAutomationEvent.h
+++ trunk/server/c/applications/machine_automation/QtMachineAutomationEvent.h
@@ -1,12 +1,12 @@
 /*
 * Copyright 2010 Rasmus Toftdahl Olesen <rasmus@sequanto.com>
-* 
+*
 * Licensed under the Apache License, Version 2.0 (the "License"); you
 * may not use this file except in compliance with the License.
 * You may obtain a copy of the License at
-* 
+*
 *     http://www.apache.org/licenses/LICENSE-2.0
-* 
+*
 * Unless required by applicable law or agreed to in writing, software
 * distributed under the License is distributed on an "AS IS" BASIS,
 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
@@ -64,11 +64,11 @@ namespace sequanto
          inline Command command () const { return m_command; }
          inline int index() const { return m_index; }
          inline const QByteArray & object() const { return m_object; }
-         
+
          bool normal() const;
          QWidget * widget() const;
          QByteArray extra() const;
-         
+
          void received ();
 
          virtual ~QtMachineAutomationEvent ();
@@ -79,17 +79,26 @@ namespace sequanto
 
       class QtMachineAutomationMouseEvent : public QEvent
       {
+      public:
+         enum Command
+         {
+             CLICK_AT,
+             HOLD_AT,
+             RELEASE_AT,
+         };
+
       private:
+         Command m_command;
          QPoint m_position;
          Qt::MouseButton m_button;
 
       public:
          static const int ID;
-
+         inline const Command command() const { return m_command; }
          inline const QPoint position () const { return m_position; }
          inline Qt::MouseButton button () const { return m_button; }
 
-         QtMachineAutomationMouseEvent ( int _x, int _y, int _button );
+         QtMachineAutomationMouseEvent ( Command _command, int _x, int _y, int _button );
 
          virtual ~QtMachineAutomationMouseEvent();
       };
--- trunk/server/c/applications/machine_automation/mouse.c
+++ trunk/server/c/applications/machine_automation/mouse.c
@@ -3,3 +3,11 @@
 void mouse_click_at ( int _x, int _y, int _button )
 {
 }
+
+void mouse_hold_at  ( int _x, int _y, int _button )
+{
+}
+
+void mouse_release_at ( int _x, int _y, int _button )
+{
+}
